a8ed0fd31d014c2f9c975860981bcda90c0ec218
Meesa000 <104768879+Meesa000@users.noreply.github.com>
2025-01-12 17:48:29 +0000

    add more commands

--- command_handler.py
+++ command_handler.py
@@ -1,6 +1,8 @@
+import random
 import discord
-import google.generativeai as genai
 from ai_response import AIResponse
+from discord.ext import commands
+from discord import app_commands
 
 
 class CommandHandler:
@@ -12,11 +14,14 @@ class CommandHandler:
 
         elif message.content.startswith('!info'):
             embed = discord.Embed(title="Wok u Won Bot", description="Information for the wok u won bot - this is a bot"
-                                                                     "that is used to help me learn the discord.py API.",
+                                                                     " that is used to help me learn the discord.py API.",
                                   colour=discord.Colour.default())
             embed.add_field(name="Commands", value="!hi\n!info\n!kosmi\n!pin\n!ask then your query to ask ai "
                                                    "something (please do not spam"
-                                                   "due to rate limits")
+                                                   "due to rate limits\n!flip to flip a coin")
+            embed.add_field(name='Features', value='This bot has an OSRS loot tracker, in built Google gemini AI.\n'
+                                                   '\nTo use the loot tracker, install the RL plugin Discord Loot Logger,'
+                                                   'and put in the webhook URL that is pinned' )
             embed.set_footer(text="Bot created by Meesa")
             await message.reply(embed=embed)
 
@@ -30,3 +35,13 @@ class CommandHandler:
         elif message.content.startswith('!ask'):
             response = AIResponse.get_ai_response(str(message.content))
             await message.reply(response)
+
+        elif message.content.startswith('!flip'):
+            coin = ['Heads', 'Tails']
+            side = random.randint(0, 1)
+            await message.reply(file=discord.File('C:\\Users\\asim_\\IdeaProjects\\wuw-bot\\images\\de-fi-land-coin-flip.gif'))
+            await message.reply(f'The coin landed on: {coin[side]}')
+
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import aiohttp
 import discord
 from command_handler import CommandHandler
 from dotenv import load_dotenv
@@ -5,19 +6,27 @@ import os
 
 load_dotenv('C:\\Users\\asim_\\IdeaProjects\\wuw-bot\\config.env')
 TOKEN = os.getenv('TOKEN')
-commands = CommandHandler()
+
+commands_handler = CommandHandler()
 
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print(f'Logged on as {self.user}')
-        channel = client.get_channel(1038401470900482130)
+        await self.change_presence(activity=discord.Game('Wok u Won Simulator'))
+        webhook_url = 'https://discord.com/api/webhooks/1328044402471604325/c6vKUKbsLCwln8f7n5cfRejX8RLNoK' \
+                      '-WmbTRiaZkCD6f3-P05EjRnhQvVWRIFAMwzyvY'
+        async with aiohttp.ClientSession() as session:
+            webhook = discord.Webhook.from_url(webhook_url, session=session)
+            await webhook.send('OSRS loot tracker is now active')
+
+        channel = self.get_channel(1038401470900482130)
         await channel.send('Wok u Won bot is online! !info for a commands list!')
 
     async def on_message(self, message):
         if message.author == client.user:
             return
-        await commands.on_message(message)
+        await commands_handler.on_message(message)
 
 
 intents = discord.Intents.default()
